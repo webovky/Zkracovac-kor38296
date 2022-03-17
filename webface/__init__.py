0920c0e5349c408e3453631bbf4ee4e709ba3673
whatisnameandwhy <kor38296@spseol.cz>
2022-03-17 13:42:59 +0100

    first commit

---
+++ webface/__init__.py
@@ -0,0 +1,13 @@
+import flask
+
+
+
+app = flask.Flask(__name__)
+
+app.secret_key = b"totoj e zceLa n@@@hodny retezec nejlep os.urandom(24)"
+app.secret_key = (
+    b"x6\x87j@\xd3\x88\x0e8\xe8pM\x13\r\xafa\x8b\xdbp\x8a\x1f\xd41\xb8"
+)
+
+from . import routes
+from . import models
